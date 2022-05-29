84eaec96166254a9bc981745b3603f4359048fd7
EmperorJake <68182631+EmperorJake@users.noreply.github.com>
2022-05-29 23:57:11 +1000

    24 series recode and hopper improvements

--- pybuild.bat
+++ pybuild.bat
@@ -2,6 +2,6 @@
 :start
 py nml_compiler.py -f "jplw.pnml" -o "jplw.nml"
 nmlc jplw.nml -o jplw.grf 
-move /y C:\Users\Jakob\Games\GitHub\JPengines\jplw.grf C:\Users\Jakob\Games\OpenTTD-JGR-+\newgrf\jakesdevland
+
 pause
 goto start
